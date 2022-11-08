f65421412ac74dcfee5d260635346be2120529d0
aurelia <91609052+Arekksu@users.noreply.github.com>
2022-11-08 17:33:42 +0900

    Add files via upload

---
+++ app/screens/HowTo.js
@@ -0,0 +1,44 @@
+// Added 20221105
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+function HowTo({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>There are several Note-Taking app, But this app is the simpliest one.
+
+Go to the note page and start your writing!
+If there are lots of notes already, go to search bar and search any keywords or word of your work.
+You can delete and edit notes too! Your work will be saved and you'll see the date on when you did that work
+ </Text>
+ <Text></Text>
+ <Text>Pages: NOTE, HOW TO USE, ABOUT</Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text></Text>
+ <Text>Want to Ask our developers for help? Go to the About Page and click Developer Profiles and send them a message via email.</Text>
+
+
+    </View>   
+  );
+}
+
+export default HowTo;
